6e44ecc96ea107b67e8f12e57aca63672d50a670
kooyooman <kooyooman@gmail.com>
2020-05-06 14:03:12 +0700

    complete lecturer and schedule

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Main from './components/Main';
 import store from './store';
 import { Provider } from 'react-redux';
-import Schedule from './components/Schedule/Schedule';
+import Testing from './testing'
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/testing.js
@@ -0,0 +1,29 @@
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from "react-component-export-image";
+import React from "react";
+class ComponentToPrint extends React.Component {
+ render() {
+   return <div>Hello World</div>;
+ }
+}
+export default class MyComponent extends React.Component {
+ constructor(props) {
+   super(props);
+   this.componentRef = React.createRef();
+ }
+
+ render() {
+   return (
+   <React.Fragment>
+       <ComponentToPrint ref={this.componentRef} />
+       <button onClick={() => exportComponentAsJPEG(this.componentRef)}>
+           Export As JPEG
+       </button>
+       <button onClick={() => exportComponentAsPDF(this.componentRef)}>
+           Export As PDF
+       </button>
+       <button onClick={() => exportComponentAsPNG(this.componentRef)}>
+           Export As PNG
+       </button>
+   </React.Fragment>);
+ }
+}
