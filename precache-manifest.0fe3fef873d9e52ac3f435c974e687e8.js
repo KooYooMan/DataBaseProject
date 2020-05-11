8a51778ebd06177e4174610a927e2cf0fe23589b
kooyooman <kooyooman@gmail.com>
2020-05-11 22:39:21 +0700

    Updates

---
+++ precache-manifest.0fe3fef873d9e52ac3f435c974e687e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b5521f804163f08f53fb125410856e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b231d7b32dcee58850d2",
+    "url": "/my-app/static/css/main.f0f72a66.chunk.css"
+  },
+  {
+    "revision": "b5d5bdc70aba89c040ac",
+    "url": "/my-app/static/js/2.4d34088a.chunk.js"
+  },
+  {
+    "revision": "eca22661d5dd9fbd8d68148ffde45888",
+    "url": "/my-app/static/js/2.4d34088a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b231d7b32dcee58850d2",
+    "url": "/my-app/static/js/main.f12cfed4.chunk.js"
+  },
+  {
+    "revision": "6f09c69fc43b61ca4d9a",
+    "url": "/my-app/static/js/runtime-main.97334edf.js"
+  },
+  {
+    "revision": "54de28187d6580da309f869c301c3b3b",
+    "url": "/my-app/static/media/lecturer.54de2818.webp"
+  },
+  {
+    "revision": "00bf7d58a407d149e25c1a0686a1373c",
+    "url": "/my-app/static/media/student.00bf7d58.jpg"
+  }
+]);
